914cdff91fc5e4df162f6ea26a00c279085ea3fd
Thomas Welfley <thomas.welfley@gmail.com>
2010-04-21 17:27:17 -0400

    fixed an issue with PyposterousError that was prevent it from outputting properly formatted error messages when an error code was specified.

--- pyposterous/error.py
+++ pyposterous/error.py
@@ -5,7 +5,7 @@ class PyposterousError(Exception):
         self.error_code = error_code
 
     def __str__(self):        
-        if error_code:
+        if self.error_code:
             return "%s - %s" % (self.error_code, self.error_message)
         
         return self.error_message
