8135787cfd5a828e79c099f051496142495f422b
Thomas Welfley <thomas.welfley@gmail.com>
2010-04-30 00:09:39 -0400

    Incremented the version number

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(name="pyposterous",
-      version="0.1",
+      version="0.2.1",
       description="Posterous library for python",
       license="MIT",
       author="Thomas Welfley",
       author_email="info@matchstrike.net",
       url="http://github.com/thomasw/pyposterous",
       dependency_links=["http://github.com/thomasw/urllib2_file/tarball/master#egg=urllib2_file",],
-      install_requires="urllib2_file",
+      install_requires=["urllib2_file",],
       packages = find_packages(),
       keywords= "posterous library",
       zip_safe = False)
