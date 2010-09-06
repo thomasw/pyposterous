3cbd673264e23fb70225c150480cf7b0ed778503
thomasw <thomas@matchstrike.net>
2010-09-06 01:02:43 -0400

    Added a class that can create a request object and add the appropriate headers for Posterous to authenticate users via Twitter's OAuth Echo implementation. This functionality currently uses the oauth2 library, but this may change before the next release.

--- pyposterous/auth.py
+++ pyposterous/auth.py
@@ -37,3 +37,50 @@ class BasicAuth(Auth):
         
         return req
         
+class TwitterAuth(Auth):
+    
+    def __init__(self, consumer_key, consumer_secret, token_key, token_secret, xauth_sp='https://api.twitter.com/1/account/verify_credentials.json'):
+        import oauth2 as oauth
+        
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
+        
+        self.token_key = token_key
+        self.token_secret = token_secret
+        self.token = oauth.Token(key=token_key, secret=token_secret)
+        
+        self.xauth_sp = xauth_sp
+        self.version = '1.0'
+        
+    def gen_request(self, url):
+        req = urllib2.Request(url)
+        
+        req.add_header("X-Auth-Service-Provider", self.xauth_sp)
+        req.add_header("X-Verify-Credentials-Authorization", self.__build_verify_credentials_string())
+        
+        return req
+    
+    def __build_verify_credentials_string(self):
+        import oauth2 as oauth
+        import time
+        
+        # Setting up a signed oauth2 Request object generates a solid verify
+        # credentials string, so we'll let it do the hard stuff for us.
+        params = oauth.Request(method="GET", url=self.xauth_sp, \
+            parameters = {
+                'oauth_version': self.version,
+                'oauth_nonce': oauth.generate_nonce(),
+                'oauth_timestamp': int(time.time()),
+                'oauth_token': self.token.key,
+                'oauth_consumer_key': self.consumer.key,
+            })
+        params.sign_request(oauth.SignatureMethod_HMAC_SHA1(), self.consumer, self.token)
+        
+        # Our verify credentials string is accessed via the 'Authorization'
+        # key. It has a bonus realm="" in it, so we remove that before we 
+        # return it.
+        return params.to_header()['Authorization'].replace(' realm="",', '')
+
+    def __unicode__(self):
+        return self.__build_verify_credentials_string()
